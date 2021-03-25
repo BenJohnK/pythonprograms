0d52c14cead207c6843a778402faf7dc90d15572
BenJohnK <benjohn.mec@gmail.com>
2021-03-25 11:56:43 +0530

    adding new files

--- javascript_programs/employee_class.js
+++ javascript_programs/employee_class.js
@@ -10,9 +10,9 @@ class Employee{
 
 var employees=[]
 var obj1=new Employee(100,"Ben","Engineer",40000)
-var obj2=new Employee(101,"Joel","Engineer",20000)
-var obj3=new Employee(100,"Lery","Sales",30000)
-var obj4=new Employee(100,"Josh","Engineer",35000)
+var obj2=new Employee(101,"Joel","Engineer",40000)
+var obj3=new Employee(102,"Lery","Sales",30000)
+var obj4=new Employee(103,"Josh","Engineer",35000)
 employees.push(obj1)
 employees.push(obj2)
 employees.push(obj3)
@@ -23,5 +23,8 @@ var employees_name_engineer=employees.filter(obj=>obj.depart=="Engineer").map(ob
 console.log(employees_name_engineer)
 var employees_name_salary=employees.filter(obj=>obj.salary>30000).map(obj=>obj.name)
 console.log(employees_name_salary)
-
+employees.sort((emp1,emp2)=>emp1.salary-emp2.salary)
+console.log(employees)
+var max_sal=employees.reduce((n1,n2)=>n1.salary>n2.salary?n1.salary:n2.salary)
+console.log(max_sal)
 
